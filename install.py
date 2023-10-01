5950a44ca2c24f70b9925f0e942f4becd4f23197
MrArsenic621 <asedighi81@gmail.com>
2023-10-01 21:34:41 +0330

    fix installs

--- install.py
+++ install.py
@@ -2,3 +2,6 @@ import launch
 
 if not launch.is_installed("nudenet"):
     launch.run_pip(f"install nudenet", "nudenet")
+
+if not launch.is_installed("diffusers"):
+    launch.run_pip(f"install diffusers", "diffusers")
